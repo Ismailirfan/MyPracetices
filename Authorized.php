2ecb1f1865e2c264d51d77d506c6159e9151b2bd
Ismail irfan <98257637+Ismailirfan@users.noreply.github.com>
2025-02-06 19:14:34 +0500

    Update Authorized.php

--- Authorized.php
+++ Authorized.php
@@ -1,1 +1,15 @@
+<?php
+require_once __DIR__ . '/facebook-sdk/autoload.php';
 
+$fb = new Facebook\Facebook([
+  'app_id' => '1769380070540366',
+  'app_secret' => '170ee5f1117544a11aa0ab64bfd7d3b4',
+  'default_graph_version' => 'v22.0',
+]);
+
+$helper = $fb->getRedirectLoginHelper();
+
+$permissions = ['email']; 
+$loginUrl = $helper->getLoginUrl('YOUR_REDIRECT_URL', $permissions);
+
+echo '<a href="' . $loginUrl . '">Log in with Facebook!</a>';
